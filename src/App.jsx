9ba1a75de253a80f06ce7943a11b4f0d23f70f15
alanjacob-exe <77325299+alanjacob-exe@users.noreply.github.com>
2023-07-18 13:12:22 +0530

    logic implemented: useReducer

---
+++ src/App.jsx
@@ -0,0 +1,110 @@
+import { useState } from "react";
+import { useReducer } from "react";
+import Card from "./assets/Components/card";
+
+
+
+
+function todoReducer(tasks, action) {
+  switch (action.type) {
+    case "added": {
+      return {
+        todo: [
+          ...tasks.todo,
+          {
+            index: tasks.todo.length,
+            content: action.text,
+          },
+        ],
+      };
+    }
+    case "edited": {
+      const updatedTasks = tasks.todo.map((task) => {
+        if (task.index === action.index) {
+          return {
+            ...task,
+            content: action.newText,
+          };
+        }
+        return task;
+      });
+
+      return {
+        todo: updatedTasks,
+      };
+
+    }
+    case "reset": {
+      return {
+        todo: []
+      };
+    }
+    default:
+      return tasks;
+  }
+}
+
+
+
+
+
+
+function App() {
+  const [tasks, dispatch] = useReducer(todoReducer, { count: 0, todo: [] });
+  const [inputvalue, setinputvalue] = useState("");
+
+  const handleTextChange = (index, newText) => {
+    dispatch({ type: 'edited', index, newText })
+  }
+  return (
+    <main className="w-screen h-screen text-white bg-black flex overdlow-hidden">
+      <div className="m-auto w-4/6 h-4/6 border-white border-2 rounded-md flex flex-col">
+        <div className="w-full h-[15%] border-white border flex content-end">
+          <div className="my-auto mx-2 font-semibold w-[20%]">Enter new Value:</div>
+
+          <div className="my-auto flex w-[30%] h-full rounded-md ml-4 ">
+            <input type="text" className="w-full h-4/6 my-auto text-black rounded-md focus:ring-2 ring-blue-500 font-semibold "
+              value={inputvalue}
+              placeholder="Enter Something"
+              onChange={(e) => setinputvalue(e.target.value)}
+            />
+
+          </div>
+          <div className="w-full my-auto flex flex-row-reverse mx-2"><button className="ml-2 w-20 h-10 focus:ring-2 focus:ring-blue-500 bg-purple-900 rounded-md transition duration-200 ease-in-out active:translate-y-1" onClick={() => {
+            if (!(inputvalue.length == 0)) {
+              { dispatch({ type: 'added', text: inputvalue }) }
+            }
+            else {
+              alert("Nothing to Insert")
+            }
+
+          }} >
+            Insert
+          </button>
+            <button className=" w-20 h-10 focus:ring-2 focus:ring-blue-500 bg-red-500 rounded-md transition duration-200 ease-in-out active:translate-y-1" onClick={() => {
+              dispatch({ type: 'reset' })
+            }} >
+              Reset
+            </button></div>
+        </div>
+        <div className="w-full h-full p-2 overflow-hidden overflow-y-auto">
+          {tasks.todo.map((value, index) => (
+            <Card key={index} value={value.content} onChange={(e) => {
+              handleTextChange(index, e.target.value)
+              // console.log("Value is:" + e.target.value)
+            }} />
+          ))}
+
+          {/* {tasks.map((eachTask, index) => (
+            <Card key={index} value={eachTask.value} />
+          ))} */}
+          {/* {dummyData.map((eachCardData, index) => (
+            <Card key={index} value={eachCardData.value} />
+          ))} */}
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/assets/Components/Card.jsx
@@ -0,0 +1,21 @@
+
+import { useState } from "react";
+
+export default function Card({ value, onChange }) {
+  const [isEditing, setisEditing] = useState(false)
+
+  return (
+    < main className="w-full h-[15%] my-1 border rounded-md text-black bg-white flex flex-row" >
+      {" "}
+      {
+        isEditing ? <input type="text" value={value} placeholder={value} onChange={onChange} onBlur={() => setisEditing(false)} className="bg-gray-200 font-semibold rounded-md h-1/2 my-auto border-black border ml-2" /> : <div className="my-auto ml-2 font-semibold" > {value}</div>
+      }
+      <div className=" flex flex-row-reverse bg-   ml-auto">
+        <button className="mx-2 my-auto w-20 h-10 focus:ring-2 focus:ring-blue-500 bg-purple-900 rounded-md transition duration-200 ease-in-out active:translate-y-1" onClick={() => setisEditing(true)}>
+          Edit
+        </button>
+        
+      </div>
+    </main >
+  );
+}
